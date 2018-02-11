87d8e4a0226fc02d776f18d90298304e8566b5a2
Masilionytė Dalia <dmasilionyte@gmail.com>
2018-02-11 16:04:52 +0200

    added comments

--- ex6a.py
+++ ex6a.py
@@ -13,6 +13,9 @@ import numpy as np
 SEPARATOR_SIZE = 1
 game_turn_number = 0
 
+
+# Function to create the game field
+
 def createGameField(configFile):
     try:
         with open(configFile, 'r', encoding = 'utf-8') as inputFile:
@@ -41,16 +44,13 @@ def createGameField(configFile):
 
                     index = 0
                     for yCoord in range(window_height):
-
                         index += 1
-
                         for xCoord in range(window_width):
                             if(xCoord % 2 == 0):
                                 board[yCoord][xCoord] = "|"
                             elif index % (symbol_edge_length + SEPARATOR_SIZE) == 0:
                                 board[yCoord][xCoord] = "-" * symbol_edge_length
                                 index = 0
-
 
                     print(np.matrix(board))
                     print("")
@@ -60,21 +60,25 @@ def createGameField(configFile):
     except FileNotFoundError:
         sys.exit ("Provided input file does not exist")  # Abort
 
+# Function to format symbol
 def getSymbol(symbol):
     symbol = symbol.replace ('"', '')
     symbol = symbol.split (', ')
     return symbol
 
+# Function to check symbol validity
 def checkValid(length, list):
     for i in range (len(list)):
         if len(list[i]) != length:
             return False
     return True
 
+# Function for creating matrices
 def createTable(width, height, element):
     table = [[element for x in range (width)] for y in range (height)]
     return table
 
+# Function to check if all the columns are filled
 def columnsNotFilled(column_fill_list):
     # If there are non filled columns the game is not over
     for column_index in column_fill_list:
@@ -83,6 +87,7 @@ def columnsNotFilled(column_fill_list):
     print("The boards is full, no player won")
     return False
 
+# Function to check for fours
 def checkForFour():
     winner = None
     for yCoord in range(len(player_track_table)):
@@ -98,7 +103,7 @@ def checkForFour():
                     winner = None
     return winner
 
-
+# Checking for fours vertically
 def verticalCheck(row, column):
     won = False
     count = 0
@@ -109,10 +114,9 @@ def verticalCheck(row, column):
             break
     if count >= 4:
         won = True
-
     return won
 
-
+# Checking for fours horizotally
 def horizontalCheck(row, column):
     won = False
     count = 0
@@ -126,13 +130,12 @@ def horizontalCheck(row, column):
 
     return won
 
-
+# Checking for fours diagonally
 def diagonalCheck(row, column):
     won = False
     count = 0
 
     # From top right to bottom left
-    #
     xCoord = column
     for yCoord in range(row, len(player_track_table)):
         if xCoord > len(player_track_table[row]) - 1:
@@ -146,6 +149,7 @@ def diagonalCheck(row, column):
     if count >= 4:
         won = True
     else:
+        # From bottom left to top right
         count = 0
         xCoord = column
         for yCoord in range(row, -1, -1):
@@ -161,47 +165,51 @@ def diagonalCheck(row, column):
 
     return won
 
-
-
-
+# Function to update game progress table
 def updateGameTrackTable(player_no, column, row):
     global game_turn_number
     player_track_table[row][column] = player_no
-    # # kodel cia local variable
     game_turn_number += 1
+    # print(np.matrix(player_track_table))
 
-    print(np.matrix(player_track_table))
-
-#### TODO GAME
+# Game turn function
 def gameTurn(player_symbol, board, column, symbol_edge_length, column_fill_list, player_track_table, player_no):
 
+    # Checking if column exists, if not function is called recursively
     if not 0 < column <= len(column_fill_list):
         print("This column does not exist")
         turn = int(input("Enter the column:"))
         gameTurn(player_symbol, board, turn, symbol_edge_length, column_fill_list, player_track_table, player_no)
+
     else:
         # Column count starts at 0
         adjusted_column = column - 1
         row = column_fill_list[adjusted_column]
+
+        # Checking if the column is full, if not the function is called recursively
         if row == 0:
             print("This column is full")
             turn = int(input("Enter the column:"))
             gameTurn(player_symbol, board, turn, symbol_edge_length, column_fill_list, player_track_table, player_no)
+
         else:
+
             adjusted_row = row * (symbol_edge_length + SEPARATOR_SIZE) - symbol_edge_length - SEPARATOR_SIZE
             for symbol in player_symbol:
                 board[adjusted_row][column * 2 - SEPARATOR_SIZE] = symbol
                 adjusted_row += 1
 
+            # Editing column fill list after a successful turn
             column_fill_list[adjusted_column] -= 1
             print(np.matrix(board))
             print("")
 
+            # Updating game progress tracking table
             updateGameTrackTable(player_no, adjusted_column, row - 1)
 
-
-
+# Function to check if game continues
 def gameContinues():
+    # Check if all columns are filled
     if columnsNotFilled(column_fill_list):
         # Start checking for fours when there already was at least 7 turns
         if game_turn_number >= 7:
@@ -216,7 +224,7 @@ def gameContinues():
     else:
         return False
 
-
+# Game
 
 board, player1_symbol, player2_symbol, symbol_edge_length, \
 player_track_table, column_fill_list = createGameField('configs.txt')
